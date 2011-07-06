cc0b508f0ca1ed98ae993073b2da6d8f943df64e
Nick Sillik <nick@sillik.org>
2011-07-05 22:59:52 -0400

    Fix a typo in the install script.

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ echo $PATH | awk -F : '{for (i=1;i<=NF;i++) {print $i}}' | grep $HOME/bin > /dev
 
 if [ $? -ne 0 ]
 	then
-		echo "Please add $HOME/bin to your \$PATH and create it if it doesn't exit"
+		echo "Please add $HOME/bin to your \$PATH and create it if it doesn't exist"
 		exit 1
 fi
+
 if [ ! -f build/Release/appledoc ]
 	then
 		echo "File ./build/Release/appledoc doesn't exist. This is unusual, do you have a special built products output dir?"
